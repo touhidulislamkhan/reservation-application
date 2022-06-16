fe6f6ebb31ca8327af26a1f61d2b58a5a83af48b
touhidulislamkhan <touhidikhan0@gmail.com>
2022-06-16 16:52:43 +0600

    home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import Featured from '../../components/Featured/Featured';
+import FeaturedProperties from '../../components/FeaturedProperties/FeaturedProperties';
+import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
+import MailList from '../../components/MailList/MailList';
 import Navbar from '../../components/Navbar/Navbar';
+import PropertyList from '../../components/PropertyList/PropertyList';
+import './home.css';
 
 const Home = () => {
 	return (
 		<div>
 			<Navbar />
 			<Header />
+			<div className="homeContainer">
+				<Featured />
+				<h1 className="homeTitle">Browse by property type</h1>
+				<PropertyList />
+				<h1 className="homeTitle">Home Guests Love</h1>
+				<FeaturedProperties />
+				<MailList />
+				<Footer />
+			</div>
 		</div>
 	);
 };
